312a02d87b430087a17ff64aa147d679f72392b4
Pavel Filípek <filipek@proclient.cz>
2019-07-10 14:31:54 +0200

    -fix reconnect v1

--- src/Queue.php
+++ src/Queue.php
@@ -164,10 +164,14 @@
           } catch (\Exception $e) {
             QueueManager::printMsg('ERROR', $e->getMessage());
             self::$db->query("UPDATE {$table->getFullName()}	SET state='" . self::STATE_ERROR . "', message='" . self::$db->escape($e->getMessage()) . "', date_end='" . date('Y-m-d H:i:s') . "' WHERE {$filterCurrentItem}");
+            
+            throw $e;
           }
         } catch (\Exception $e) {
           if ($e->getCode() == '2006') {
             self::$db->reconnect();
+          } else {
+            throw $e;
           }
         }
   
